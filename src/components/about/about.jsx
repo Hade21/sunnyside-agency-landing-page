d25763b9f5673f3c98e837c53d020d2c23248f40
Muhammad Abdurrohman <hade21@192.168.1.13>
2022-01-28 21:10:08 +0800

    can't custom color on image footer

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -9,7 +9,7 @@ const About = () => {
     return(
         <div className="wrapper bg-footer py-16">
             <img src={logo} alt="" className="mx-auto mb-7"/>
-            <div className="menu flex justify-center gap-8 md:w-1/2 mx-auto font-barlow text-base mb-16`">
+            <div className="menu flex justify-center gap-8 md:w-1/2 mx-auto font-barlow text-base mb-16">
                 <p className="text-footer-text">About</p>
                 <p className="text-footer-text">Services</p>
                 <p className="text-footer-text">Projects</p>
