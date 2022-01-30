29fa79f1a58d4569faf1ec3dd036e8f4381324ca
Hade21 <abdurrohmanmuhammad123@gmail.com>
2022-01-30 14:25:43 +0800

    add hover effect

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -8,11 +8,11 @@ import pt from '../../images/icon-pinterest.svg'
 const About = () => {
     return(
         <div className="wrapper bg-footer py-16">
-            <img src={logo} alt="" className="mx-auto mb-7"/>
+            <img src={logo} alt="" className="mx-auto mb-7" />
             <div className="menu flex justify-center gap-8 lg:w-1/2 w-full mx-auto font-barlow text-base mb-16">
-                <p className="menu-text text-footer-text cursor-pointer">About</p>
-                <p className="menu-text text-footer-text cursor-pointer">Services</p>
-                <p className="menu-text text-footer-text cursor-pointer">Projects</p>
+                <p className="hover:text-white-pallete text-footer-text cursor-pointer">About</p>
+                <p className="hover:text-white-pallete text-footer-text cursor-pointer">Services</p>
+                <p className="hover:text-white-pallete text-footer-text cursor-pointer">Projects</p>
             </div>
             <div className="sosmed flex justify-center gap-6">
                 <img src={fb} alt="facebook" className="cursor-pointer" />
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -8,14 +8,14 @@ const Navbar = () => {
             <div className="left">
                 <img src={logo} alt="logo" />
             </div>
-            <div className="right text-white flex justify-end">
-                <ul className="font-barlow md:flex justify-end gap-8 items-center hidden">
-                    <li className="menu-text cursor-pointer">About</li>
-                    <li className="menu-text cursor-pointer">Services</li>
-                    <li className="menu-text cursor-pointer">Project</li>
-                    <li className="font-fraunces text-sm px-6 py-3 bg-white-pallete text-very-dark-desaturated-blue rounded-full cursor-pointer" id="contact">CONTACT</li>
+            <div className="right lg:text-white flex justify-end">
+                <img src={hamburger} alt="hamburger-menu" className="h-fit my-auto md:hidden cursor-pointer peer" tabIndex={0} />
+                <ul className="font-barlow lg:flex flex-col lg:flex-row lg:justify-end lg:gap-8 lg:items-center peer-focus:bg-white-pallete peer-focus:w-3/4 peer-focus:absolute peer-focus:right-11 peer-focus:top-24  peer-focus:text-very-dark-grayish-blue peer-focus:text-lg peer-focus:flex hidden">
+                    <li className="menu-text cursor-pointer p-4 lg:p-0">About</li>
+                    <li className="menu-text cursor-pointer p-4 lg:p-0">Services</li>
+                    <li className="menu-text cursor-pointer p-4 lg:p-0">Project</li>
+                    <li className="font-fraunces text-sm px-6 py-3 w-fit mx-auto m-4 bg-yellow-pallete lg:bg-white-pallete text-very-dark-desaturated-blue rounded-full cursor-pointer hover:bg-blue-300 hover:text-white-pallete" id="contact">CONTACT</li>
                 </ul>
-                <img src={hamburger} alt="hamburger-menu" className="h-fit my-auto md:hidden cursor-pointer"/>
             </div>
         </nav>
     )
