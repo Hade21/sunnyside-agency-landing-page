8d175a6586d721e81425462c96d1c69cd8532edc
Hade21 <abdurrohmanmuhammad123@gmail.com>
2022-01-31 19:30:18 +0800

    Fix hover effect on share button

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,27 +1,41 @@
 import React from "react";
 import {ReactComponent as Logo} from '../../images/logo.svg';
-import fb from '../../images/icon-facebook.svg';
+import {ReactComponent as Fb} from '../../images/icon-facebook.svg';
 import {ReactComponent as Ig} from '../../images/icon-instagram.svg';
-import tw from '../../images/icon-twitter.svg';
-import pt from '../../images/icon-pinterest.svg'
+import {ReactComponent as Tw} from '../../images/icon-twitter.svg';
+import {ReactComponent as Pt} from '../../images/icon-pinterest.svg'
 
-const About = () => {
-    return(
-        <div className="wrapper bg-footer py-16">
-            <Logo fill="#2c7566" className="mx-auto mb-8" />
-            <div className="menu flex justify-center gap-8 lg:w-1/2 w-full mx-auto font-barlow text-base mb-16">
-                <p className="hover:text-white-pallete text-footer-text cursor-pointer">About</p>
-                <p className="hover:text-white-pallete text-footer-text cursor-pointer">Services</p>
-                <p className="hover:text-white-pallete text-footer-text cursor-pointer">Projects</p>
-            </div>
-            <div className="sosmed flex justify-center gap-6">
-                <img src={fb} alt="facebook" className="cursor-pointer text-footer-text hover:text-white-pallete" />
-                <Ig fill="#2c7566" className="text-footer-text hover:text-white-pallete cursor-pointer" />
-                <img src={tw} alt="twitter" className="cursor-pointer" />
-                <img src={pt} alt="pinterest" className="cursor-pointer" />
-            </div>
-        </div>
-    )
+
+class About extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            colorFb: '#2c7566',
+            colorIg: '#2c7566',
+            colorTw: '#2c7566',
+            colorPt: '#2c7566'
+        }
+    }
+
+    render() {
+        return(
+            <div className="wrapper bg-footer py-16">
+             <Logo fill="#2c7566" className="mx-auto mb-8" />
+             <div className="menu flex justify-center gap-8 lg:w-1/2 w-full mx-auto font-barlow text-base mb-16">
+                 <p className="hover:text-white-pallete text-footer-text cursor-pointer">About</p>
+                 <p className="hover:text-white-pallete text-footer-text cursor-pointer">Services</p>
+                 <p className="hover:text-white-pallete text-footer-text cursor-pointer">Projects</p>
+             </div>
+             <div className="sosmed flex justify-center gap-6">
+                <Fb  onMouseEnter={() => this.setState({colorFb: '#FFFFFF'})} onMouseLeave={() => this.setState({colorFb : "#2c7566"})} fill={this.state.colorFb} className="cursor-pointer" />
+                 <Ig  onMouseEnter={() => this.setState({colorIg: '#FFFFFF'})} onMouseLeave={() => this.setState({colorIg : "#2c7566"})} fill={this.state.colorIg} className="cursor-pointer" />
+                 <Tw  onMouseEnter={() => this.setState({colorTw: '#FFFFFF'})} onMouseLeave={() => this.setState({colorTw : "#2c7566"})} fill={this.state.colorTw} className="cursor-pointer" />
+                 <Pt  onMouseEnter={() => this.setState({colorPt: '#FFFFFF'})} onMouseLeave={() => this.setState({colorPt : "#2c7566"})} fill={this.state.colorPt} className="cursor-pointer" />
+             </div>
+         </div>
+        )
+    }
 }
+
 
 export default About;
